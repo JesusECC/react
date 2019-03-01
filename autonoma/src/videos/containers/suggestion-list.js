aabc11ab1d801e11d5fa10609258b65d13693ebc
Jesus <jesuschilca@gmail.com>
2019-03-01 09:43:25 -0500

    modificaciones finales del sieño, creando utilidades

--- autonoma/src/videos/containers/suggestion-list.js
+++ autonoma/src/videos/containers/suggestion-list.js
@@ -7,10 +7,17 @@ import{
 import Layout from '../components/suggestion-list-layouts';
 import Empty from '../components/empty';
 import Separator from '../components/vertical-separator';
+import Suggestion from '../components/suggestion';
+
 
 class SuggestionList extends Component{
   renderEmtpy=()=><Empty text="No hay sugerencias :(" />
   itemSeparator=()=><Separator />
+  renderItem=({item})=>{
+    return(
+      <Suggestion {...item} />
+    )
+  }
   render() {
     const list=[
       {
@@ -29,7 +36,7 @@ class SuggestionList extends Component{
           data={list}
           ListEmptyComponent={this.renderEmtpy}
           ItemSeparatorComponent={this.itemSeparator}
-          renderItem={({item}) => <Text>{item.title}</Text>}
+          renderItem={this.renderItem}
         />
       </Layout>
     )
